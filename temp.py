86cb1602098749fb308dd0d37b23a97c6ab26579
zhouke <small-crocodile@sohu.com>
2015-02-02 15:20:20 +0800

    20150202

--- temp.py
+++ temp.py
@@ -1,43 +1,8 @@
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*- 
 
-import random
-
-l = [random.randint(1,100) for i in range(20)]
-
+l = list(''' "[{"a":226,"b":4330016,"c":321,"d":333565,"e":1,"f":3}
+            ,{"a":209,"b":4220011,"c":147,"d":333564,"e":1,"f":2}]" ''')
 print(l)
-
-def bubbleSort(data):
-    if len(data) < 2: return data
-    for i in range(0, len(data)-1):
-        m = i
-        for j in range(i+1, len(data)):
-            if data[m] > data[j]: m =j
-
-        if m != i:
-            data[i], data[m] = data[m], data[i]
-    return data
-
-
-def insertSort(data):
-    if len(data) < 2: return data
-    for i in range(1,len(data)):
-        print data
-        key = data[i]
-        j = i-1
-        while j >= 0 and key < data[j]:
-            data[j+1] = data[j]
-            j = j-1
-        data[j+1]= key
-    return data
-
-
-l1 = l[:]
-bubbleSort(l1)
-
-l2 = l[:]
-insertSort(l2)
-
-print(l1)
-
-print(l2)
+t = eval('[{"a":226,"b":4330016,"c":321,"d":333565,"e":1,"f":3},{"a":209,"b":4220011,"c":147,"d":333564,"e":1,"f":2}]')
+print t[0]
